9cfdf3885c93f9bdb4beb1c6210808a4e28f89a0
Dikhi martin <dikhi.martin@gmail.com>
2020-11-18 19:42:16 +0700

    Myprofile

--- app/routes/my_profile_route.go
+++ app/routes/my_profile_route.go
@@ -8,6 +8,6 @@ import (
 func MyProfile(g *echo.Group) {
 	DEFINE_URL := "/my_profile"
 	g.GET(DEFINE_URL +"/", controllers.MyProfileController)
-	// g.POST(DEFINE_URL+"/update_inline/:type_post/", controllers.UpdateInlineProfile)
+	g.POST(DEFINE_URL+"/update_inline/:field/", controllers.UpdateInlineProfile)
 	g.POST(DEFINE_URL+"/confirm_update/", controllers.UpdateProfileController)
 }
